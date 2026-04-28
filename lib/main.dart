cdbf4414e2de6ad60924d498066d15b7acc1c4f7
Matheus <florestiferrari@gmail.com>
2026-04-27 21:42:46 -0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/ex7/loginPage.dart';
+import 'package:flutter_app/detalhesPage.dart' show DetalhesPage;
+import 'package:flutter_app/homePage.dart' show HomePage;
+import 'package:flutter_app/loginPage.dart';
+import 'package:go_router/go_router.dart';
+final _roteador = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => LoginPage(),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => HomePage(),
+    ),
+  ],
+);
 
 void main() {
   runApp(
-    MaterialApp(
-      home: LoginPage(),
+    MaterialApp.router(
+      routerConfig: _roteador,
       debugShowCheckedModeBanner: false,
     ),
   );
